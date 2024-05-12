cd0d4e259d0d03d2f85ca47f41ced4cdf5711706
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-05-12 15:20:36 +0900

    [FEAT] Home화면 화면 이동 구현

--- app/src/main/java/com/kiosk/app/HomeActivity.kt
+++ app/src/main/java/com/kiosk/app/HomeActivity.kt
@@ -1,11 +1,23 @@
 package com.kiosk.app
 
+import android.content.Intent
 import android.os.Bundle
 import com.kiosk.app.databinding.ActivityHomeBinding
 import com.sopt.instagram.util.binding.BindingActivity
+import com.sopt.instagram.util.extension.setOnSingleClickListener
 
 class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.btnStore.setOnSingleClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnTakeOut.setOnSingleClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
