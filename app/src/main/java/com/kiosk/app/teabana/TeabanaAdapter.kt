639e2c103349f70a04e305e704e3ac48ebdb07eb
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-05-21 17:29:50 +0900

    [FEAT] tebana recyclerview adapter -> listadapter

--- app/src/main/java/com/kiosk/app/teabana/TeabanaAdapter.kt
+++ app/src/main/java/com/kiosk/app/teabana/TeabanaAdapter.kt
@@ -2,30 +2,52 @@ package com.kiosk.app.teabana
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.kiosk.app.Item
+import com.kiosk.app.MainViewModel
 import com.kiosk.app.databinding.ItemTeabanaMenuBinding
+import com.kiosk.app.util.DiffCallback
+import com.sopt.instagram.util.extension.setOnSingleClickListener
 
-class TeabanaAdapter(private val items: List<Item>) :
-    RecyclerView.Adapter<TeabanaAdapter.ViewHolder>() {
+class TeabanaAdapter(
+    private val viewModel: MainViewModel,
+) : ListAdapter<Item, TeabanaAdapter.TeabanaViewHolder>(diffUtil) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemTeabanaMenuBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeabanaViewHolder {
+        val binding =
+            ItemTeabanaMenuBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TeabanaViewHolder(binding, viewModel)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(items[position])
+    override fun onBindViewHolder(holder: TeabanaViewHolder, position: Int) {
+        holder.bind(getItem(position))
     }
 
-    override fun getItemCount() = items.size
+    fun addItems(newItems: List<Item>) {
+        val currentItems = currentList.toMutableList()
+        currentItems.addAll(newItems)
+        submitList(currentItems)
+    }
 
-    class ViewHolder(private val binding: ItemTeabanaMenuBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    class TeabanaViewHolder(
+        private val binding: ItemTeabanaMenuBinding,
+        private val viewModel: MainViewModel,
+    ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: Item) {
             binding.ivTeabanaMenu.setImageResource(item.image)
             binding.tvTeabanaName.text = item.name
             binding.tvTeabanaPrice.text = item.price
+            binding.layoutTeabanaMenu.setOnSingleClickListener {
+                viewModel.setSelectedItem(item)
+            }
         }
+    }
+
+    companion object {
+        private val diffUtil = DiffCallback<Item>(
+            onItemsTheSame = { old, new -> old == new }, // Assuming Item has an 'id' field
+            onContentsTheSame = { old, new -> old == new },
+        )
     }
 }
--- app/src/main/java/com/kiosk/app/teabana/TeabanaFragment.kt
+++ app/src/main/java/com/kiosk/app/teabana/TeabanaFragment.kt
@@ -1,40 +1,35 @@
 package com.kiosk.app.teabana
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.kiosk.app.Item
+import com.kiosk.app.MainViewModel
 import com.kiosk.app.R
 import com.kiosk.app.databinding.FragmentTeabanaBinding
+import com.kiosk.app.util.binding.BindingFragment
 
-class TeabanaFragment : Fragment() {
+class TeabanaFragment : BindingFragment<FragmentTeabanaBinding>(R.layout.fragment_teabana) {
 
-    private var _binding: FragmentTeabanaBinding? = null
-    private val binding get() = _binding!!
+    private val viewModel by viewModels<MainViewModel>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = FragmentTeabanaBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+    private var _adapter: TeabanaAdapter? = null
+    private val adapter
+        get() = requireNotNull(_adapter) { "adapter_not_initialized_error_msg" }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val recyclerView = binding.rvTeabana
-        recyclerView.layoutManager = GridLayoutManager(context, 4)
-        recyclerView.adapter = TeabanaAdapter(getItems())
+        binding.rvTeabana.layoutManager = GridLayoutManager(context, 4)
+        _adapter = TeabanaAdapter(viewModel)
+        binding.rvTeabana.adapter = adapter
+        val items = getItems()
+        adapter.submitList(items)
     }
 
     private fun getItems(): List<Item> {
         return listOf(
-            Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
+            Item(R.drawable.ic_main_delete, "xlqkek 라떼", "3500원"),
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
@@ -44,10 +39,5 @@ class TeabanaFragment : Fragment() {
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
         )
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
