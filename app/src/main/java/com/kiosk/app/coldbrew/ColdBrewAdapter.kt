3c51f169e8bfaf13a46a185d1312c1d1675ca39c
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-05-21 17:19:01 +0900

    [FEAT] coldbrew recyclerview adapter -> listadapter

--- app/src/main/java/com/kiosk/app/coldbrew/ColdBrewAdapter.kt
+++ app/src/main/java/com/kiosk/app/coldbrew/ColdBrewAdapter.kt
@@ -2,31 +2,46 @@ package com.kiosk.app.coldbrew
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.kiosk.app.Item
+import com.kiosk.app.MainViewModel
 import com.kiosk.app.databinding.ItemColdbrewMenuBinding
+import com.kiosk.app.util.DiffCallback
 
-class BlendedAdapter(private val items: List<Item>) :
-    RecyclerView.Adapter<BlendedAdapter.ViewHolder>() {
+class ColdBrewAdapter(
+    private val viewModel: MainViewModel,
+) : ListAdapter<Item, ColdBrewAdapter.ViewHolder>(diffUtil) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemColdbrewMenuBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemColdbrewMenuBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false,
+        )
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(items[position])
+        holder.bind(getItem(position))
     }
 
-    override fun getItemCount() = items.size
+    override fun getItemCount() = currentList.size
 
     class ViewHolder(private val binding: ItemColdbrewMenuBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
         fun bind(item: Item) {
             binding.ivColdbrewMenu.setImageResource(item.image)
             binding.tvColdbrewName.text = item.name
             binding.tvColdbrewPrice.text = item.price
         }
     }
-}
 
+    companion object {
+        private val diffUtil = DiffCallback<Item>(
+            onItemsTheSame = { old, new -> old == new }, // Assuming Item has an 'id' field
+            onContentsTheSame = { old, new -> old == new },
+        )
+    }
+}
--- app/src/main/java/com/kiosk/app/coldbrew/ColdBrewFragment.kt
+++ app/src/main/java/com/kiosk/app/coldbrew/ColdBrewFragment.kt
@@ -1,36 +1,30 @@
 package com.kiosk.app.coldbrew
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.kiosk.app.Item
+import com.kiosk.app.MainViewModel
 import com.kiosk.app.R
-import com.kiosk.app.databinding.FragmentBlendedBinding
 import com.kiosk.app.databinding.FragmentColdbrewBinding
+import com.kiosk.app.util.binding.BindingFragment
 
-class ColdBrewFragment : Fragment() {
+class ColdBrewFragment : BindingFragment<FragmentColdbrewBinding>(R.layout.fragment_coldbrew) {
+    private val viewModel by viewModels<MainViewModel>()
 
-    private var _binding: FragmentColdbrewBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = FragmentColdbrewBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+    private var _adapter: ColdBrewAdapter? = null
+    private val adapter
+        get() = requireNotNull(_adapter) { "adapter_not_initialized_error_msg" }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView = binding.rvColdbrew
-        recyclerView.layoutManager = GridLayoutManager(context, 4)
-        recyclerView.adapter = BlendedAdapter(getItems())
+        binding.rvColdbrew.layoutManager = GridLayoutManager(context, 4)
+        _adapter = ColdBrewAdapter(viewModel)
+        binding.rvColdbrew.adapter = adapter
+        val items = getItems()
+        adapter.submitList(items)
     }
 
     private fun getItems(): List<Item> {
@@ -45,10 +39,5 @@ class ColdBrewFragment : Fragment() {
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
         )
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
