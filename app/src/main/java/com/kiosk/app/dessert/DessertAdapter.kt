4e108d0ba8d018dd3cbba1c9ff5333bb4c6f940a
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-05-21 17:26:29 +0900

    [FEAT] dessert recyclerview adapter -> listadapter

--- app/src/main/java/com/kiosk/app/dessert/DessertAdapter.kt
+++ app/src/main/java/com/kiosk/app/dessert/DessertAdapter.kt
@@ -2,31 +2,53 @@ package com.kiosk.app.dessert
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.kiosk.app.Item
+import com.kiosk.app.MainViewModel
 import com.kiosk.app.databinding.ItemDessertMenuBinding
+import com.kiosk.app.util.DiffCallback
+import com.sopt.instagram.util.extension.setOnSingleClickListener
 
-class DessertAdapter(private val items: List<Item>) :
-    RecyclerView.Adapter<DessertAdapter.ViewHolder>() {
+class DessertAdapter(
+    private val viewModel: MainViewModel,
+) : ListAdapter<Item, DessertAdapter.DessertViewHolder>(diffUtil) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DessertViewHolder {
         val binding =
             ItemDessertMenuBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+        return DessertViewHolder(binding, viewModel)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(items[position])
+    override fun onBindViewHolder(holder: DessertViewHolder, position: Int) {
+        holder.bind(getItem(position))
     }
 
-    override fun getItemCount() = items.size
+    fun addItems(newItems: List<Item>) {
+        val currentItems = currentList.toMutableList()
+        currentItems.addAll(newItems)
+        submitList(currentItems)
+    }
 
-    class ViewHolder(private val binding: ItemDessertMenuBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    class DessertViewHolder(
+        private val binding: ItemDessertMenuBinding,
+        private val viewModel: MainViewModel,
+    ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: Item) {
             binding.ivDessertMenu.setImageResource(item.image)
             binding.tvDessertName.text = item.name
             binding.tvDessertPrice.text = item.price
+            binding.layoutDessertMenu.setOnSingleClickListener {
+                viewModel.setSelectedItem(item)
+            }
         }
     }
-}
+
+        companion object {
+            private val diffUtil = DiffCallback<Item>(
+                onItemsTheSame = { old, new -> old == new }, // Assuming Item has an 'id' field
+                onContentsTheSame = { old, new -> old == new },
+            )
+        }
+    }
+
--- app/src/main/java/com/kiosk/app/dessert/DessertFragment.kt
+++ app/src/main/java/com/kiosk/app/dessert/DessertFragment.kt
@@ -1,40 +1,36 @@
 package com.kiosk.app.dessert
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.kiosk.app.Item
+import com.kiosk.app.MainViewModel
 import com.kiosk.app.R
 import com.kiosk.app.databinding.FragmentDessertBinding
+import com.kiosk.app.util.binding.BindingFragment
 
-class DessertFragment : Fragment() {
+class DessertFragment : BindingFragment<FragmentDessertBinding>(R.layout.fragment_dessert) {
 
-    private var _binding: FragmentDessertBinding? = null
-    private val binding get() = _binding!!
+    private val viewModel by viewModels<MainViewModel>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = FragmentDessertBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+    private var _adapter: DessertAdapter? = null
+    private val adapter
+        get() = requireNotNull(_adapter) { "adapter_not_initialized_error_msg" }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView = binding.rvDessert
-        recyclerView.layoutManager = GridLayoutManager(context, 4)
-        recyclerView.adapter = DessertAdapter(getItems())
+        binding.rvDessert.layoutManager = GridLayoutManager(context, 4)
+        _adapter = DessertAdapter(viewModel)
+        binding.rvDessert.adapter = adapter
+        val items = getItems()
+        adapter.submitList(items)
     }
 
     private fun getItems(): List<Item> {
         return listOf(
-            Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
+            Item(R.drawable.ic_main_delete, "g기 라떼", "3500원"),
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
@@ -44,10 +40,5 @@ class DessertFragment : Fragment() {
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
             Item(R.drawable.ic_main_delete, "딸기 라떼", "3500원"),
         )
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
