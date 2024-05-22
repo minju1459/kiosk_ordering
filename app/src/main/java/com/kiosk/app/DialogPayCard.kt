77b9b2848c76fc8aa14a8c2287fda9ca25d8409e
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-05-22 16:35:54 +0900

    [FEAT] 카드 결제 승인 요청 시 토스트 메시지

--- app/src/main/java/com/kiosk/app/DialogPayCard.kt
+++ app/src/main/java/com/kiosk/app/DialogPayCard.kt
@@ -1,10 +1,14 @@
 package com.kiosk.app
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.kiosk.app.databinding.DialogPayCardBinding
 import com.kiosk.app.util.binding.BindingDialogFragment
+import com.sopt.instagram.util.extension.setOnSingleClickListener
 
 class DialogPayCard(private val totalPrice: Int) : BindingDialogFragment<DialogPayCardBinding>(R.layout.dialog_pay_card) {
 
@@ -13,5 +17,13 @@ class DialogPayCard(private val totalPrice: Int) : BindingDialogFragment<DialogP
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.tvAmount.text = totalPrice.toString()
+
+        binding.btnApprove.setOnSingleClickListener {
+            Toast.makeText(this.context, "결제 승인 요청 중입니다.", Toast.LENGTH_LONG).show()
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                Toast.makeText(this.context, "결제가 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            }, 3000)
+        }
     }
 }
